dc1ffd29c855adaef8229711814a495ca987e627
SerkanKiran <92333465+SerkanKiran@users.noreply.github.com>
2021-10-14 20:44:01 +0200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using BNG;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YakTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_GetYakMilk()
+        {
+            YakInfo yak = new YakInfo();
+            int age = 4;
+            int days = 13;
+            decimal expected = (decimal)491.6600;
+            decimal yakMilk = yak.GetYakMilk(age, days);
+            Assert.AreEqual(expected, yakMilk);
+        }
+
+        [TestMethod]
+        public void Test_GetYakWool()
+        {
+            YakInfo yak = new YakInfo();
+            int age = 4;
+            int days = 20;
+            int expected = 2;
+            int yakWool = yak.GetYakWool(age, days);
+            Assert.AreEqual(expected, yakWool);
+        }
+    }
+}
---
+++ Yak1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml;
+using BNG;
+
+namespace Yak_1
+{
+    class Yak1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The XML file to read");
+            XmlDocument doc = new XmlDocument();
+            string xmlFile = Console.ReadLine();
+            doc.Load(xmlFile);
+            XmlNodeList yaks = doc.SelectNodes("herd/labyak");
+            Console.WriteLine("elapsed days");
+            int days = Int32.Parse(Console.ReadLine());
+
+            decimal yakAge;
+            int wool = 0;
+            decimal milk = 0;
+            YakInfo yakInfo = new YakInfo();
+            Console.WriteLine("Herd:");
+            decimal ageAfter;
+            string name;
+            Console.WriteLine("Herd:");
+            foreach (XmlNode yak in yaks)
+            {
+                yakAge = decimal.Parse(yak.Attributes["age"].Value, System.Globalization.CultureInfo.InvariantCulture);
+                name = yak.Attributes["name"].Value;
+                milk = milk + yakInfo.GetYakMilk(yakAge, days);
+                wool = wool + yakInfo.GetYakWool(yakAge, days);
+                ageAfter = yakAge + ((decimal)0.01 * days);
+                Console.WriteLine("{0} {1} years old", name, ageAfter);
+            }
+
+            Console.WriteLine("In Stock:");
+            Console.WriteLine("{0} liters of milk", milk);
+            Console.WriteLine("{0} skins of wool", wool);
+
+        }
+    }
+}
